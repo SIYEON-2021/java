cbccfbaa904608cddbc61ed61732bbaf942f6983
SIYEON-2021 <76863050+SIYEON-2021@users.noreply.github.com>
2021-01-07 21:30:22 +0900

    Add files via upload
    변수 코드 예제 적용

---
+++ VarEx2.java
@@ -0,0 +1,18 @@
+
+public class VarEx2 {
+
+	public static void main(String[] args) {
+	
+	char ch= 'A';
+	int i ='A';
+	
+	String str = ""; // ºó ¹®ÀÚ¿­(empty string)
+	String str2= "ABCD";
+	String str3= "123";
+	String str4= str2+ str3;
+	System.out.println(str4);
+	System.out.println(""+7+7);
+	System.out.println(7+7+"");
+	}
+
+}
