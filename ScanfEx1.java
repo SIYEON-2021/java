d2754f8fd92f46de1c3531dd3bb6d3564774befe
SIYEON_2021 <siso7955@gmail.com>
2021-01-09 14:20:00 +0900

    Add File [ScanfEx1]

---
+++ ScanfEx1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner; //1. import 문 추가
+//'' java.util.*;
+public class ScanfEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		//scanner class의 객체 생성
+		
+		int num= scanner.nextInt();
+		int num2= scanner.nextInt();
+		
+		System.out.println(num);
+		System.out.println(num2);
+		
+//		String input= scanner.nextLine();
+//		int num= Integer.parseInt(input);
+//		
+//		System.out.println(num);
+//		
+//		
+		
+	}
+
+}
